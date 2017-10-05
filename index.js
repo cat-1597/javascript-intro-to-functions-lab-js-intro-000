884126918e05d1f7768a484533255118fa802a85
Cat <cchiong1597@gmail.com>
2017-10-05 01:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function shout(string) {
+function logShout(string) {
   console.log().toUpperCase()
 }
 function sayHiToGrandma(string) {
