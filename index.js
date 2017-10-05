ed37f79d212cc27c8fb9ad0b2bc3bce1591e4f26
Cat <cchiong1597@gmail.com>
2017-10-05 01:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,3 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUppercase())
 }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
-  return "I can't hear you!"
-}
-  if (string.toUppercase() === string)
-  return "Yes Indeed!"
-}
-  else if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-}
